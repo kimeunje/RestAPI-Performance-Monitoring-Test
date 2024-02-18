f533a811c1ec07de7fa4f8e8125785c4c47dbbc4
world.wide.eunseo <world.wide.eunseo@gmail.com>
2024-02-18 22:19:33 +0900

    Refactor: 테이블 생성 sql 수정

--- createTable.sql
+++ createTable.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        11.1.2-MariaDB - mariadb.org binary distribution
+-- 호스트:                          localhost
+-- 서버 버전:                        10.11.2-MariaDB - mariadb.org binary distribution
 -- 서버 OS:                        Win64
 -- HeidiSQL 버전:                  12.3.0.6589
 -- --------------------------------------------------------
@@ -16,20 +16,42 @@
 
 
 -- monitor 데이터베이스 구조 내보내기
+DROP DATABASE IF EXISTS `monitor`;
 CREATE DATABASE IF NOT EXISTS `monitor` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `monitor`;
 
 -- 테이블 monitor.items 구조 내보내기
+DROP TABLE IF EXISTS `items`;
 CREATE TABLE IF NOT EXISTS `items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `imgPath` varchar(100) NOT NULL,
+  `img_path` varchar(100) DEFAULT NULL,
   `price` int(11) NOT NULL DEFAULT 0,
-  `discounterPer` int(11) NOT NULL DEFAULT 0,
+  `discount_per` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 monitor.items:~2 rows (대략적) 내보내기
+DELETE FROM `items`;
+INSERT INTO `items` (`id`, `name`, `img_path`, `price`, `discount_per`) VALUES
+	(4, 'starry', '/img/starry-8450454_1280.jpg', 100000, 3),
+	(5, 'watercolour', '/img/watercolour-2168729_1280.jpg', 250000, 5),
+	(6, 'vincent van gogh', '/img/vincent-van-gogh-85799_1280.jpg', 420000, 30);
+
+-- 테이블 monitor.members 구조 내보내기
+DROP TABLE IF EXISTS `members`;
+CREATE TABLE IF NOT EXISTS `members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 monitor.members:~1 rows (대략적) 내보내기
+DELETE FROM `members`;
+INSERT INTO `members` (`id`, `email`, `password`) VALUES
+	(1, 'test@goo.com', 'test1234');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
