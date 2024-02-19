acbd9745d119f93810b4f36e2ee87fc23dc871f1
world.wide.eunseo <world.wide.eunseo@gmail.com>
2024-02-19 22:45:29 +0900

    Feat: 장바구니 추가 및 조회 API 생성

--- backend/src/main/java/org/project/monitor/backend/Repository/ItemRepository.java
+++ backend/src/main/java/org/project/monitor/backend/Repository/ItemRepository.java
@@ -1,10 +1,13 @@
 package org.project.monitor.backend.Repository;
 
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 import org.project.monitor.backend.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
-
+  List<Item> findByIdIn(List<Integer> ids);
 }
---
+++ backend/src/main/java/org/project/monitor/backend/entity/Cart.java
@@ -0,0 +1,27 @@
+package org.project.monitor.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column
+  private int memberId;
+
+  @Column
+  private int itemId;
+}
