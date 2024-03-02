5f131bb9d90911d13c9b5ba6cbb78e934d343c50
world.wide.eunseo <world.wide.eunseo@gmail.com>
2024-03-02 19:27:29 +0900

    Feat: 로그아웃시 쿠키 삭제 기능 추가

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,11 +1,13 @@
 <script setup>
 import store from '@/scripts/store';
 import router from '@/scripts/router';
+import axios from 'axios';
 
 const logout = () => {
-  store.commit('setAccount', 0);
-  sessionStorage.removeItem('id');
-  router.push({ path: '/' });
+  axios.post('/api/account/logout').then(() => {
+    store.commit('setAccount', 0);
+    router.push({ path: '/' });
+  });
 };
 </script>
 
@@ -21,13 +23,20 @@ const logout = () => {
                 <router-link to="/" class="text-white">메인 화면</router-link>
               </li>
               <li v-if="$store.state.account.id">
-                <router-link to="/orders" class="text-white">주문 내역</router-link>
+                <router-link to="/orders" class="text-white"
+                  >주문 내역</router-link
+                >
               </li>
               <li>
-                <router-link to="/login" class="text-white" v-if="!$store.state.account.id">
+                <router-link
+                  to="/login"
+                  class="text-white"
+                  v-if="!$store.state.account.id"
+                >
                   로그인
                 </router-link>
-                <a to="/login" class="text-white" @click="logout()" v-else>로그아웃
+                <a to="/login" class="text-white" @click="logout()" v-else
+                  >로그아웃
                 </a>
               </li>
             </ul>
@@ -38,10 +47,22 @@ const logout = () => {
     <div class="navbar navbar-dark bg-dark shadow-sm">
       <div class="container">
         <router-link to="/" class="navbar-brand d-flex align-items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
-            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2"
-            viewBox="0 0 24 24">
-            <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            aria-hidden="true"
+            class="me-2"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"
+            />
             <circle cx="12" cy="13" r="4" />
           </svg>
           <strong>Gallery</strong>
@@ -49,8 +70,15 @@ const logout = () => {
         <router-link to="cart" class="cart btn">
           <i class="fa fa-shopping-cart" aria-hidden="true"></i>
         </router-link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
-          aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarHeader"
+          aria-controls="navbarHeader"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
       </div>
@@ -59,7 +87,6 @@ const logout = () => {
 </template>
 
 <style scoped>
-
 header ul li a {
   cursor: pointer;
 }
